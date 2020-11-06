484e873930f72718127b596372fb85719119e16e
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-06 17:16:17 +0300

    Initial commit

--- src/main/java/com/giggle/team/TeamApplication.java
+++ src/main/java/com/giggle/team/TeamApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeamApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TeamApplication.class, args);
     }
-
 }
