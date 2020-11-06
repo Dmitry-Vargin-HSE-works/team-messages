b5e75d0cc6528ba8470b5fd5ca7092840b2992e3
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-06 17:41:32 +0300

    initial preparing for boot

--- src/main/java/com/giggle/team/TeamApplication.java
+++ src/main/java/com/giggle/team/TeamApplication.java
@@ -2,8 +2,9 @@ package com.giggle.team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
 public class TeamApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamApplication.class, args);
