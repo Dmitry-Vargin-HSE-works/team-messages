b28fb38bc6f2120efe0528f49a7a2f4ec0996988
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-17 00:56:03 +0300

    update comments

--- src/main/java/com/giggle/team/config/SecurityConfiguration.java
+++ src/main/java/com/giggle/team/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.giggle.team.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,27 +13,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableConfigurationProperties
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    /*
+    /**
      * more about spring security
      * https://mainul35.medium.com/spring-mvc-spring-security-in-memory-user-details-configuration-90d106b53d23
      */
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/auth").permitAll()
+                .antMatchers("/", "/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable()
                 .formLogin().disable();
     }
 
-
+    /**
+     * about password encoders
+     * https://reflectoring.io/spring-security-password-handling/
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // about password encoders
-        // https://reflectoring.io/spring-security-password-handling/
         return new BCryptPasswordEncoder(10);
     }
 
