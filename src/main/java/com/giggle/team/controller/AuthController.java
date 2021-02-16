08888e003c528bcaac78fd8624ef408b730a3591
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-17 00:57:48 +0300

    add datasource auth

--- src/main/java/com/giggle/team/controller/AuthController.java
+++ src/main/java/com/giggle/team/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.giggle.team.controller;
 
+import com.giggle.team.models.User;
+import com.giggle.team.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +20,19 @@ public class AuthController {
 
     private final PasswordEncoder encoder;
     private final AuthenticationManagerBuilder builder;
+    private final UserRepository userRepository;
 
     public AuthController(PasswordEncoder bCryptPasswordEncoder,
-                          AuthenticationManagerBuilder authenticationManagerBuilder) {
+                          AuthenticationManagerBuilder authenticationManagerBuilder,
+                          UserRepository userRepository) {
         this.encoder = bCryptPasswordEncoder;
         this.builder = authenticationManagerBuilder;
+        this.userRepository = userRepository;
     }
 
     /**
      * in memory authentication
+     *
      * @param user - get the description from GET request
      * @return Http status 200 or 500
      */
@@ -49,8 +54,35 @@ public class AuthController {
         }
     }
 
+
+    /**
+     * database authentication
+     *
+     * @param user - get the description from GET request
+     * @return Http status 200 or 500
+     */
+    @RequestMapping(value = "database", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<String> userPostDatabase(@RequestBody User user) {
+        try {
+            userRepository.save(user);
+            builder.inMemoryAuthentication()
+                    .withUser(user.getUsername())
+                    .password(encoder.encode(user.getPassword()))
+                    .roles("USER", "ADMIN")
+                    .accountExpired(false)
+                    .accountLocked(false)
+                    .credentialsExpired(false)
+                    .disabled(false);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     /**
      * an example of user for post request
+     *
      * @return User json representation
      */
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
