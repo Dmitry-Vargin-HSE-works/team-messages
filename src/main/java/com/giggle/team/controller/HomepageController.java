04f66307d2d57f2e9f168ef5f5e3ee659c7e44a6
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-04-22 00:40:55 +0300

    implemented basic sign in logic

--- src/main/java/com/giggle/team/controller/HomepageController.java
+++ src/main/java/com/giggle/team/controller/HomepageController.java
@@ -1,20 +1,31 @@
 package com.giggle.team.controller;
 
-import com.giggle.team.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomepageController {
 
-    @GetMapping(value = "/homepage")
-    public ModelAndView homepage() {
-        ModelAndView modelAndView = new ModelAndView();
-        User user = new User();
-        modelAndView.addObject("user", user);
-        modelAndView.setViewName("homepage");
-        return modelAndView;
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String loginform() {
+        return "loginform.html";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String login(@ModelAttribute(name = "email") String email,
+                        @ModelAttribute(name = "password") String password,
+                        Model model) {
+        if ("spring".equals(email) && "root".equals(password))
+            return "im";
+        return "";
+    }
+
+    @RequestMapping(value = "im", method = RequestMethod.GET)
+    public String chatform() {
+        return "chatform.html";
     }
 
 }
