dd30cbd6b6f132bf37136582bb52faeb70f20de0
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-05-25 20:32:51 +0300

    added new method for receiving list of users

--- src/main/java/com/giggle/team/repositories/UserRepository.java
+++ src/main/java/com/giggle/team/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.giggle.team.repositories;
 
 import com.giggle.team.models.UserEntity;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ public interface UserRepository extends MongoRepository<UserEntity, ObjectId> {
     UserEntity findByEmail(String email);
 
     UserEntity findByUsername(String username);
+
+    Page<UserEntity> findByUsernameStartsWith(String query);
 }
