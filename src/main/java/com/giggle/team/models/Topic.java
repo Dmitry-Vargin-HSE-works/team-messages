421238a2f58db67f4a5c52f096a29bfa5049e3c5
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-05-26 10:26:30 +0300

    added new fields in Topic

--- src/main/java/com/giggle/team/models/Topic.java
+++ src/main/java/com/giggle/team/models/Topic.java
@@ -5,10 +5,14 @@ import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
+import java.util.TreeSet;
+
 @Getter
 @Setter
 public class Topic {
     @Id
     private ObjectId id;
+    private TreeSet<UserEntity> users;
+    private String kafkaTopic;
     private String stompDestination;
 }
