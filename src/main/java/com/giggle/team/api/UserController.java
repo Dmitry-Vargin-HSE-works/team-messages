c7244811b314a233ccde89534cf857bf0c3852d2
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-05-28 19:53:28 +0300

    revert java style

--- src/main/java/com/giggle/team/api/UserController.java
+++ src/main/java/com/giggle/team/api/UserController.java
@@ -18,45 +18,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Objects;
 
 @RequestMapping("/api/v1/users")
 @RestController
 public class UserController {
 
-  public static final String phrase = "example";
+    public static final String phrase = "example";
 
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @Autowired
-  private TopicRepository topicRepository;
+    @Autowired
+    private TopicRepository topicRepository;
 
-  @Autowired
-  private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  @Transactional
-  @JsonView(View.Rest.class)
-  @RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json")
-  public ResponseEntity<String> signup(@RequestBody UserEntity userEntity) {
-    if (Objects.isNull(userRepository.findByEmail(userEntity.getEmail()))) {
-      userEntity.setPassword(bCryptPasswordEncoder.encode(userEntity.getPassword()));
-      Topic main = topicRepository.findByStompDestination("main");
-      main.addUser(userEntity);
-      //userEntity.setTopics(Collections.singletonList(main)); // fixme temp crutch
-      userRepository.save(userEntity);
-      topicRepository.save(main);
-      return new ResponseEntity<>("User created", HttpStatus.OK);
+    @Transactional
+    @JsonView(View.Rest.class)
+    @RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<String> signup(@RequestBody UserEntity userEntity) {
+        if (Objects.isNull(userRepository.findByEmail(userEntity.getEmail()))) {
+            userEntity.setPassword(bCryptPasswordEncoder.encode(userEntity.getPassword()));
+            Topic main = topicRepository.findByStompDestination("main");
+            main.addUser(userEntity);
+            //userEntity.setTopics(Collections.singletonList(main)); // fixme temp crutch
+            userRepository.save(userEntity);
+            topicRepository.save(main);
+            return new ResponseEntity<>("User created", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Email exists", HttpStatus.CONFLICT);
     }
-    return new ResponseEntity<>("Email exists", HttpStatus.CONFLICT);
-  }
 
-  @JsonView(View.Rest.class)
-  @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
-  public ResponseEntity<UserEntity> entity() {
-    return ResponseEntity.ok(new UserEntity(phrase, phrase, phrase));
-  }
+    @JsonView(View.Rest.class)
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<UserEntity> entity() {
+        return ResponseEntity.ok(new UserEntity(phrase, phrase, phrase));
+    }
 
     /*@JsonView(View.Messages.class)
     @RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json")
@@ -64,8 +63,8 @@ public class UserController {
         return userRepository.findByUsernameStartsWith(query, pageable);
     }*/
 
-  @RequestMapping(value = "/chats", method = RequestMethod.GET, produces = "application/json")
-  public Page<Topic> chats(Pageable pageable) {
-    return topicRepository.findAll(pageable);
-  }
+    @RequestMapping(value = "/chats", method = RequestMethod.GET, produces = "application/json")
+    public Page<Topic> chats(Pageable pageable) {
+        return topicRepository.findAll(pageable);
+    }
 }
